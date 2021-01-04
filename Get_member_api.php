84a6796231be8005d8e9299122602101da590757
peeraphatw <judiciarypz@gmail.com>
2021-01-04 16:48:40 +0700

    sqladded

---
+++ Get_member_api.php
@@ -0,0 +1,79 @@
+<?php
+    header('Content-Type: application/json');
+    class PDOCONFIG extends PDO{
+        public $member  = array();
+        public function __construct()
+        {
+            try
+            {
+                parent::__construct("mysql:host=localhost;dbname=ry_db",'root','',
+                array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+                $this->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+                $this->setAttribute(PDO::ATTR_EMULATE_PREPARES,FALSE);
+                
+            }
+            catch
+            (PDOExpection $e)
+            {
+                echo $e->getMessage();
+            }
+           
+
+        }
+
+        public function OpenAPI()
+        {
+            $data = $this->prepare('SELECT * FROM Register_tb');
+            $data->execute();
+            $members = array();
+            while($output = $data->fetch(PDO::FETCH_ASSOC))
+            {
+                $members[$output['id']] = array(
+                    'id' => $output['id'],
+                    'username' => $output['username'],
+                    'email' => $output['email'],
+                    'password' =>$output['password'],
+                    'gender' => $output['gender'],
+                    'age'=> $output['age'],
+                    'country' => $output['country'],
+                    'dateofbirth' => $output['dateofbirth'],
+                    'phone' => $output['tel'],
+                    'term' => $output['term'],
+                    'letter' => $output['letter']
+                );
+            }
+            return json_encode($members);
+        }
+    }
+
+    // class memberApi{
+    //     public function GetRequest()
+    //     {
+    //         $connection = new PDOCONFIG;
+    //         $users = array();
+    //         $data = $connection->prepare('SELECT * FROM Register_tb');
+    //         $data->execute();
+    //         //print_r($data->fetch(PDO::FETCH_ASSOC));
+
+    //         //echo json_encode($data->fetch(PDO::FETCH_ASSOC));
+    //         //while($output = $data->fetch())
+    //     }
+    // }
+
+  
+
+if($_SERVER['REQUEST_METHOD'] === 'GET')
+{
+    $apiSelf = new PDOCONFIG;
+    echo $apiSelf->OpenAPI();
+}else
+{
+    echo json_encode($_SERVER['REQUEST_METHOD']."  Method not allowed");
+}
+
+
+    
+    
+    
+
+    
---
+++ ry_db.sql
@@ -0,0 +1,78 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 04, 2021 at 10:46 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ry_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `register_tb`
+--
+
+CREATE TABLE `register_tb` (
+  `id` int(10) NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(10) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `gender` varchar(1) NOT NULL,
+  `age` int(2) NOT NULL,
+  `country` varchar(30) NOT NULL,
+  `dateofbirth` date NOT NULL,
+  `tel` varchar(10) NOT NULL,
+  `term` varchar(5) NOT NULL,
+  `letter` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `register_tb`
+--
+
+INSERT INTO `register_tb` (`id`, `username`, `password`, `email`, `gender`, `age`, `country`, `dateofbirth`, `tel`, `term`, `letter`) VALUES
+(1, 'Peeraphatw', 'test', 'Judiciarypz@gmail.com', 'm', 23, 'thailand', '2021-01-04', '944368295', 'on', 'on'),
+(2, 'Peeraphatw2', 'test2', 'Judiciarypz@gmail.com', 'm', 23, 'thailand', '2021-01-04', '944368295', 'on', 'on'),
+(3, 'Peeraphatw3', 'test3', 'Judiciarypz@gmail.com', 'm', 23, 'thailand', '2021-01-04', '944368295', 'on', 'on'),
+(4, 'Peeraphatw4', 'test4', 'Judiciarypz@gmail.com', 'm', 23, 'thailand', '2021-01-04', '944368295', 'on', 'on'),
+(5, 'Peeraphatw', 'test', 'Judiciarypz@gmail.com', 'm', 23, 'thailand', '2021-01-04', '944368295', 'on', 'on');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `register_tb`
+--
+ALTER TABLE `register_tb`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `register_tb`
+--
+ALTER TABLE `register_tb`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
